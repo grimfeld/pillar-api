a1315a30342a169f68321475acb7b92445372edb
Grim's Desktop <paulperson.pro@gmail.com>
2022-09-23 21:18:56 +0200

    Added mongoose config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
+import { ConfigService } from './config/config.service';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule,
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.getMongoConfig(),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import { ConfigService } from './config/config.service';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const config = new ConfigService();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(await config.getPortConfig());
 }
 bootstrap();
